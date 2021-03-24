b9e7b04b04d85d653309b7662e14b8c5135ff981
benoit.lapointe.3@ens.etsmtl.ca <Benoit Lapointe>
2021-03-24 12:04:38 -0400

    Fixed a typo in the urls mapping

--- jopro/project/project/urls.py
+++ jopro/project/project/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('JobPortal.urls'))
+    path('', include('app.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
