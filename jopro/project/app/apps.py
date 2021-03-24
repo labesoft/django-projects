aa01aa0f3e436cbeb3834e6c6e6946e32cf730bb
benoit.lapointe.3@ens.etsmtl.ca <Benoit Lapointe>
2021-03-24 12:08:50 -0400

    Fixed the app registering which haven't been done.

--- jopro/project/app/apps.py
+++ jopro/project/app/apps.py
@@ -1,5 +1,22 @@
+"""The apps of the Job Portal Web Application
+-----------------------------
+
+Project structure
+-----------------
+*jopro/project/app/*
+    **apps.py**:
+        The apps of the Job Portal Web Application
+
+About this Module
+------------------
+The goal of this module is to map the models to the installed apps.
+"""
+
+__author__ = "Benoit Lapointe"
+__date__ = "2021-03-24"
+__copyright__ = "Copyright 2021, labesoft"
+__version__ = "1.0.0"
+
 from django.apps import AppConfig
 
-
-class AppConfig(AppConfig):
-    name = 'app'
+AppConfig.name = 'app'
